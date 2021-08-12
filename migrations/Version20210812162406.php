f9e3afe59e81fb430dc71d1c11f2d3ef966b9d36
florence prin <florenceprin@orange.fr>
2021-08-12 16:33:45 +0200

    ajout jeu de données pour les départements qui seront disponibles dans la choicelist du champ "départements" du formulaire

---
+++ migrations/Version20210812162406.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210812162406 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('INSERT INTO departements (nom, email) VALUES
+        (\'RH\',\'rh@efficience-it.com\'),  
+        (\'commerciaux\',\'commerce@efficience-it.com\'),
+        (\'informations\',\'info@efficience-it.com\'),
+        (\'IT\',\'it@efficience-it.com\');
+        ');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('TRUNCATE TABLE departements');
+    }
+}
