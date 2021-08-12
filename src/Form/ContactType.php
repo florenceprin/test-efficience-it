46e6cefe1fa2e183cfc4af9ae59ee626df1ea0ac
florence prin <florenceprin@orange.fr>
2021-08-12 16:10:50 +0200

    correction formulaire et ajout bouton d'envoi

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -2,8 +2,13 @@
 
 namespace App\Form;
 
+use App\Entity\Departements;
 use App\Entity\FicheContact;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -17,12 +22,13 @@ class ContactType extends AbstractType
             ->add('Prenom',TextType::class,[
                 'label'=> 'Prénom',
             ])
-            ->add('Email',MailType::class)
+            ->add('Email',EmailType::class)
             ->add('Message',TextAreaType::class)
             ->add('Departement',EntityType::class, [
                 'label'=> 'Départements',
                 'class'=> Departements::class,
             ])
+            ->add('Envoyer',SubmitType::class)
         ;
     }
 
