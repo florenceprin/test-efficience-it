fae050711b9e5f20f8ad467eceb473990328c21b
florence prin <florenceprin@orange.fr>
2021-08-12 18:00:38 +0200

    suppression commentaires dans repositories

--- src/Repository/DepartementsRepository.php
+++ src/Repository/DepartementsRepository.php
@@ -19,32 +19,5 @@ class DepartementsRepository extends ServiceEntityRepository
         parent::__construct($registry, Departements::class);
     }
 
-    // /**
-    //  * @return Departements[] Returns an array of Departements objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('d')
-            ->andWhere('d.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('d.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
 
-    /*
-    public function findOneBySomeField($value): ?Departements
-    {
-        return $this->createQueryBuilder('d')
-            ->andWhere('d.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
--- src/Repository/FicheContactRepository.php
+++ src/Repository/FicheContactRepository.php
@@ -19,32 +19,5 @@ class FicheContactRepository extends ServiceEntityRepository
         parent::__construct($registry, FicheContact::class);
     }
 
-    // /**
-    //  * @return FicheContact[] Returns an array of FicheContact objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('c')
-            ->andWhere('c.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('c.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
 
-    /*
-    public function findOneBySomeField($value): ?FicheContact
-    {
-        return $this->createQueryBuilder('c')
-            ->andWhere('c.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
